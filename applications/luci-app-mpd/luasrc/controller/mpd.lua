41ddbf2f7aa1334d6630e051ca0e7caf04eaa65a
Martin Matějek <martin.matejek@gmx.com>
2019-12-19 17:22:08 +0100

    fixup! WIP: luci-app-mpd: initial functionality

--- applications/luci-app-mpd/luasrc/controller/mpd.lua
+++ applications/luci-app-mpd/luasrc/controller/mpd.lua
@@ -1,10 +1,9 @@
 module("luci.controller.mpd", package.seeall)
 
 function index()
-        if not nixio.fs.access("/etc/config/mpd") then
-                return
-        end
-
-        entry({"admin", "services", "mpd"}, firstchild(), _("MPD"), 30).dependent=false
-        entry({"admin", "services", "mpd", "tab_from_cbi"}, cbi("mpd"), _("CBI tab"), 30)
+	if not nixio.fs.access("/etc/config/mpd") then
+		return
+	end
+	
+	entry({"admin", "services", "mpd"}, cbi("mpd"), _("MPD server"), 30).dependent=true
 end
--- applications/luci-app-mpd/luasrc/model/cbi/mpd.lua
+++ applications/luci-app-mpd/luasrc/model/cbi/mpd.lua
@@ -1,9 +1,18 @@
-m = Map("mpd", "MPD")
+m = Map("mpd", "MPD server",
+	"Music Player Daemon (MPD) is a flexible, powerful, server-side application for playing music.")
 
-s = m:section(TypedSection, "settings", "Settings")
-s.addremove = true
+s = m:section(TypedSection, "mpd", "MPD server settings")
+s.anonymous = true
 
-foo = s:option(Value, "foo", "Foo", "foo value, usually bar")
-foo.optional = true
+enabled = s:option(Flag, "enabled", "Enable")
+
+port = s:option(Value, "port", "Port", "Listening port (default: 6600)")
+music_directory = s:option(Value, "music_directory", "music_directory") 
+playlist_directory = s:option(Value, "playlist_directory", "playlist_directory")
+db_file = s:option(Value, "db_file", "db_file")
+log_file = s:option(Value, "log_file", "log_file")
+pid_file = s:option(Value, "pid_file", "pid_file")
+state_file = s:option(Value, "state_file", "state_file")
+sticker_file = s:option(Value, "sticker_file", "sticker_file")
 
 return m
